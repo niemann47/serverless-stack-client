da49443a1d76d57f2afd64deef581bb996441a65
Nick Niemann <niemann47@gmail.com>
2020-04-09 21:34:17 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-1",
+    BUCKET: "hoot"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://p55i1uet4c.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_u8Awr9C4E",
+    APP_CLIENT_ID: "Y73e52guli62m68951b0mdh6p9e",
+    IDENTITY_POOL_ID: "us-west-2:0d4f5ac1-2d21-4178-95c4-cec4ba7f92b5"
+  }
+};
